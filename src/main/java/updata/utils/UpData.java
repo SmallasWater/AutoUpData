9bc4267736dab168e6c262acdfa2c169d01c5b4d
LT_Name <572413378@qq.com>
2023-11-21 22:25:54 +0800

    支持版本后缀检查

--- src/main/java/updata/utils/UpData.java
+++ src/main/java/updata/utils/UpData.java
@@ -7,15 +7,14 @@ import cn.nukkit.utils.ConfigSection;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import updata.AutoData;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 自动更新代码
@@ -237,36 +236,68 @@ public class UpData {
      * 比较版本
      */
     private int compareVersion(String v1, String v2) {
-        if (v1.equals(v2)) {
+        if (v1.equalsIgnoreCase(v2)) {
             return 0;
         }
-        String[] version1Array = v1.split("[._]");
-        String[] version2Array = v2.split("[._]");
-        int index = 0;
-        int minLen = Math.min(version1Array.length, version2Array.length);
-        long diff = 0;
+        try {
+            LinkedList<String> version1Array = new LinkedList<>(Arrays.asList(v1.split("[._-]")));
+            LinkedList<String> version2Array = new LinkedList<>(Arrays.asList(v2.split("[._-]")));
 
-        while (index < minLen
-                && (diff = Long.parseLong(version1Array[index])
-                - Long.parseLong(version2Array[index])) == 0) {
-            index++;
-        }
-        if (diff == 0) {
-            for (int i = index; i < version1Array.length; i++) {
-                if (Long.parseLong(version1Array[i]) > 0) {
-                    return 1;
+            LinkedList<String> version1Suffix = new LinkedList<>();
+            LinkedList<String> version2Suffix = new LinkedList<>();
+
+            int v1index = version1Array.size() - 1;
+            while (v1index != 0) {
+                try {
+                    Long.parseLong(version1Array.getLast());
+                }catch (Exception e) {
+                    version1Suffix.addLast(version1Array.pollLast());
                 }
+                v1index--;
+            }
+            int v2index = version2Array.size() - 1;
+            while (v2index != 0) {
+                try {
+                    Long.parseLong(version2Array.getLast());
+                }catch (Exception e) {
+                    version2Suffix.addLast(version2Array.pollLast());
+                }
+                v2index--;
             }
 
-            for (int i = index; i < version2Array.length; i++) {
-                if (Long.parseLong(version2Array[i]) > 0) {
+            int index = 0;
+            int minLen = Math.min(version1Array.size(), version2Array.size());
+            long diff = 0;
+
+            while (index < minLen
+                    && (diff = Long.parseLong(version1Array.get(index))
+                    - Long.parseLong(version2Array.get(index))) == 0) {
+                index++;
+            }
+            if (diff == 0) {
+                if (!version1Suffix.equals(version2Suffix)) {
                     return -1;
                 }
+
+                for (int i = index; i < version1Array.size(); i++) {
+                    if (Long.parseLong(version1Array.get(i)) > 0) {
+                        return 1;
+                    }
+                }
+
+                for (int i = index; i < version2Array.size(); i++) {
+                    if (Long.parseLong(version2Array.get(i)) > 0) {
+                        return -1;
+                    }
+                }
+                return 0;
+            } else {
+                return diff > 0 ? 1 : -1;
             }
-            return 0;
-        } else {
-            return diff > 0 ? 1 : -1;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return -1;
     }
 
     private static void download(String urlPath, String targetDirectory, String fileName) throws Exception {
